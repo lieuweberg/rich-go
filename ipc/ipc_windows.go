6e2a1a8b2d9e10fd5c00b17bf7b3721f882c95ee
lieuweberg <36763108+lieuweberg@users.noreply.github.com>
2021-06-18 22:36:15 +0200

    try connecting to pipes 0, 1 and 2

--- ipc/ipc_windows.go
+++ ipc/ipc_windows.go
@@ -3,8 +3,12 @@
 package ipc
 
 import (
-	npipe "gopkg.in/natefinch/npipe.v2"
+	"errors"
+	"fmt"
+	"log"
 	"time"
+
+	npipe "gopkg.in/natefinch/npipe.v2"
 )
 
 // OpenSocket opens the discord-ipc-0 named pipe
@@ -12,11 +16,19 @@ func OpenSocket() error {
 	// Connect to the Windows named pipe, this is a well known name
 	// We use DialTimeout since it will block forever (or very very long) on Windows
 	// if the pipe is not available (Discord not running)
-	sock, err := npipe.DialTimeout(`\\.\pipe\discord-ipc-0`, time.Second*2)
-	if err != nil {
-		return err
+	for i := range make([]rune, 3) {
+		sock, err := npipe.DialTimeout(fmt.Sprintf(`\\.\pipe\discord-ipc-%d`, i), time.Second*2)
+		if err != nil {
+			if errors.As(err, &npipe.PipeError{}) {
+				continue
+			}
+			return err
+		} else {
+			socket = sock
+			log.Printf("Connected to pipe %d", i)
+			return nil
+		}
 	}
 
-	socket = sock
-	return nil
+	return errors.New("unable to connect to pipes 0, 1 or 2")
 }
